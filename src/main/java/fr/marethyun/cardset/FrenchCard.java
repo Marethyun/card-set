6cf9084da8101d713e219c1674715829230c744a
Misbeliever <bacci.ange@gmail.com>
2018-10-27 22:31:20 +0200

    Added french card set

--- src/main/java/fr/marethyun/cardset/FrenchCard.java
+++ src/main/java/fr/marethyun/cardset/FrenchCard.java
@@ -1,6 +1,6 @@
 package fr.marethyun.cardset;
 
-public final class FrenchCard {
+public final class FrenchCard implements Card {
 
     private final Color color;
     private final Type type;
@@ -8,6 +8,10 @@ public final class FrenchCard {
     public FrenchCard(Color color, Type type) {
         this.color = color;
         this.type = type;
+    }
+
+    public FrenchCard() {
+        this(Color.NONE, Type.NONE);
     }
 
     public Color getColor() {
@@ -19,28 +23,65 @@ public final class FrenchCard {
     }
 
     public enum Color {
-        NONE,
-        CLUBS,
-        SPADES,
-        DIAMONDS,
-        HEARTS
+        NONE("#"),
+        CLUBS("♣"),
+        SPADES("♠"),
+        DIAMONDS("♦"),
+        HEARTS("♥");
+
+        private final String color;
+
+        Color(String color) {
+            this.color = color;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        @Override
+        public String toString() {
+            return getColor();
+        }
     }
 
     public enum Type {
-        NONE,
-        ACE,
-        ONE,
-        TWO,
-        THREE,
-        FOUR,
-        FIVE,
-        SIX,
-        SEVEN,
-        NINE,
-        TEN,
-        JACK,
-        QUEEN,
-        KING,
-        JOKER
+        NONE(0, "#"),
+        ACE(1, "A"),
+        TWO(2, "2"),
+        THREE(3, "3"),
+        FOUR(4, "4"),
+        FIVE(5, "5"),
+        SIX(6, "6"),
+        SEVEN(7, "7"),
+        EIGHT(8, "8"),
+        NINE(9, "9"),
+        TEN(10, "10"),
+        JACK(11, "V"),
+        QUEEN(12, "Q"),
+        KING(13, "K"),
+        JOKER(14, "J");
+
+        private final int type;
+        private final String descriptor;
+
+        // TODO Add a descriptor char
+        Type(int type, String descriptor) {
+            this.type = type;
+            this.descriptor = descriptor;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public String getDescriptor() {
+            return descriptor;
+        }
+
+        @Override
+        public String toString() {
+            return Integer.toString(getType());
+        }
     }
 }
