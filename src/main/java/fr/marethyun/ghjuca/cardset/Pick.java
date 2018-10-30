ae6f471bcdeaf7145bd82003f8f2608e37dcfa56
Misbeliever <bacci.ange@gmail.com>
2018-10-30 11:42:19 +0100

    Implemented swap and shuffle methods

--- src/main/java/fr/marethyun/ghjuca/cardset/Pick.java
+++ src/main/java/fr/marethyun/ghjuca/cardset/Pick.java
@@ -2,11 +2,23 @@ package fr.marethyun.ghjuca.cardset;
 
 import fr.marethyun.ghjuca.Shuffleable;
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Pick extends ArrayDeque<Card> implements Shuffleable {
+public class Pick extends ArrayList<Card> implements Shuffleable {
     @Override
     public void shuffle() {
-        // TODO: Shuffle
+        Random random = new Random();
+        for (int i = this.size(); i > 1; i--){
+            swap(i - 1, random.nextInt(i));
+        }
+    }
+
+    public final void swap(int firstIndex, int secondIndex){
+        Card first = this.get(firstIndex);
+        Card second = this.get(secondIndex);
+
+        this.set(firstIndex, second);
+        this.set(secondIndex, first);
     }
 }
